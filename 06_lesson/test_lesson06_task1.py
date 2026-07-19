fd65cf57c83965c8085161f6630fabcb359cb719
maximovaarina2016-glitch <maximova.arina2016@gmail.com>
2026-07-19 22:52:40 +0300

    Third commit

--- 06_lesson/test_lesson06_task1.py
+++ 06_lesson/test_lesson06_task1.py
@@ -3,36 +3,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome()
+def test_lesson06_task1():
 
-url = "https://the-internet.herokuapp.com/dynamic_loading/2"
-driver.get(url)
+    driver = webdriver.Chrome()
 
-
-start_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, "#start button"))
-)
-start_button.click()
+    url = "https://the-internet.herokuapp.com/dynamic_loading/2"
+    driver.get(url)
 
 
-hello_world_element = WebDriverWait(driver, 20).until(
-    EC.visibility_of_element_located((By.ID, "finish"))
-)
+    start_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "#start button"))
+    )
+    start_button.click()
 
 
-actual_text = hello_world_element.text.strip()
-
-
-if actual_text == "Hello World!":
-    print("Проверка пройдена: Текст совпадает.")
-else:
-    print(
-        f"Проверка провалена: Ожидалось 'Hello World!', получено '{actual_text}'"
+    hello_world_element = WebDriverWait(driver, 20).until(
+        EC.visibility_of_element_located((By.ID, "finish"))
     )
 
 
-screenshot_path = "screenshot_dynamic_loading.png"
-driver.save_screenshot(screenshot_path)
-print(f"Скриншот сохранен как {screenshot_path}")
+    actual_text = hello_world_element.text.strip()
 
-driver.quit()
+
+    if actual_text == "Hello World!":
+        print("Проверка пройдена: Текст совпадает.")
+    else:
+        print(
+            f"Проверка провалена: Ожидалось 'Hello World!', получено '{actual_text}'"
+        )
+
+
+    screenshot_path = "screenshot_dynamic_loading.png"
+    driver.save_screenshot(screenshot_path)
+    print(f"Скриншот сохранен как {screenshot_path}")
+
+    driver.quit()
--- 06_lesson/test_lesson06_task2.py
+++ 06_lesson/test_lesson06_task2.py
@@ -3,74 +3,71 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome()
+def test_lesson06_task2():
 
+    driver = webdriver.Chrome()
 
-driver.get("https://www.gitflic.ru/")
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.TAG_NAME, "footer"))
-)
+    driver.get("https://www.gitflic.ru/")
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, "footer"))
+    )
 
-# Karoline_max
-# kJDXZjr!Zmn4TT6
-user1 = driver.add_cookie(
-    {
+    # Karoline_max
+    # kJDXZjr!Zmn4TT6
+
+user1 = driver.add_cookie
+    ({
         "name": "SESSION",
         "value": "MTI2NzQzMGEtNTAxYi00ZjAxLWI4YzAtMTBlZWY5MjFlZTg2",
         "domain": "gitflic.ru",
-    }
-)
-driver.add_cookie(
-    {"name": "cookiesAccepted", "value": "true", "domain": "gitflic.ru"}
-)
+    })
+    driver.add_cookie({
+        "name": "cookiesAccepted",
+        "value": "true",
+        "domain": "gitflic.ru"
+    })
 
-driver.get("https://gitflic.ru/user/karoline_max")
-user1_header = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.CSS_SELECTOR, ".user-profile__name"))
-)
-url_user1 = driver.current_url
-print(f"Первый пользователь загружен: {url_user1}")
+    driver.get("https://gitflic.ru/user/karoline_max")
+    user1_header = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, ".user-profile__name"))
+    )
+    url_user1 = driver.current_url
+    print(f"Первый пользователь загружен: {url_user1}")
 
-driver.delete_all_cookies()
-driver.refresh()
+    driver.delete_all_cookies()
+    driver.refresh()
 
-driver.get("https://www.gitflic.ru/")
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.TAG_NAME, "footer"))
-)
+    driver.get("https://www.gitflic.ru/")
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, "footer"))
+    )
 
-# Arina_yudushkina
-# CQ7-mFizBD79Qx5
-user2 = driver.add_cookie(
-    {
+    # Arina_yudushkina
+    # CQ7-mFizBD79Qx5
+    user2 = driver.add_cookie({
         "name": "SESSION",
         "value": "MWM0NTdjOWEtZDRmMS00YWZhLWI4ZTctNDhjYjcyNzk2OTA0",
         "domain": "gitflic.ru",
-    }
-)
-driver.add_cookie(
-    {"name": "cookiesAccepted", "value": "true", "domain": "gitflic.ru"}
-)
+    })
+    driver.add_cookie({
+        "name": "cookiesAccepted",
+        "value": "true",
+        "domain": "gitflic.ru"
+    })
 
-driver.get("https://gitflic.ru/user/arina_yudushkina")
+    driver.get("https://gitflic.ru/user/arina_yudushkina")
 
-user2_header = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.CSS_SELECTOR, ".user-profile__name"))
-)
-
-url_user2 = driver.current_url
-print(f"Второй пользователь загружен: {url_user2}")
-
-if (
-    url_user1 != url_user2
-    and "karoline_max" in url_user1
-    and "arina_yudushkina" in url_user2
-):
-    assert True
-    print("Проверка пройдена: открыты страницы разных пользователей.")
-else:
-    raise AssertionError(
-        f"Проверка провалена. URL1: {url_user1}, URL2: {url_user2}"
+    user2_header = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, ".user-profile__name"))
     )
 
-driver.quit()
+    url_user2 = driver.current_url
+    print(f"Второй пользователь загружен: {url_user2}")
+
+    if url_user1 != url_user2 and "karoline_max" in url_user1 and "arina_yudushkina" in url_user2:
+        assert True
+        print("Проверка пройдена: открыты страницы разных пользователей.")
+    else:
+        raise AssertionError(f"Проверка провалена. URL1: {url_user1}, URL2: {url_user2}")
+
+    driver.quit()
